ffc9685f74f5bb7c26057299f0656bd93c568dcb
Hagershawket <hagershawkat@gmail.com>
2024-10-20 10:09:26 +0300

    feat(user): add edit action to User Controller to manage the roles.

---
+++ LinkDev.Talabat.Dashboard/ViewModels/Role/RoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkDev.Talabat.Dashboard.ViewModels.Role
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ LinkDev.Talabat.Dashboard/ViewModels/User/UpdatedUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LinkDev.Talabat.Dashboard.ViewModels.Role;
+
+namespace LinkDev.Talabat.Dashboard.ViewModels.User
+{
+    public class UpdatedUserViewModel
+    {
+        public string? UserName { get; set; } = null!;
+        public List<RoleViewModel> Roles { get; set; } = null!;
+    }
+}

0d80c1519d192bad08a5c932eb1a9c7ae234f8cf
Hagershawket <hagershawkat@gmail.com>
2024-10-20 06:30:01 +0300

    feat(role): add edit action to Role Controller.

--- LinkDev.Talabat.Dashboard/Controllers/RoleController.cs
+++ LinkDev.Talabat.Dashboard/Controllers/RoleController.cs
@@ -44,6 +44,49 @@ namespace LinkDev.Talabat.Dashboard.Controllers
 
         #endregion
 
+        #region Edit
+
+        [HttpGet]   // GET: /Role/Edit/id?
+        public async Task<IActionResult> Edit(string? id)
+        {
+            if (id is null)
+                return BadRequest();
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role is null)
+                return NotFound();
+
+            return View(new RoleFormViewModel { Name = role.Name! });
+        }
+
+        [HttpPost] // POST
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromRoute] string id, RoleFormViewModel model)
+        {
+            if (id is null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var roleExist = await _roleManager.FindByNameAsync(model.Name);
+            if (roleExist is not null && roleExist.Id != id)            
+            {
+                ModelState.AddModelError("Name", "This Role is already Exist");
+                return View(model);
+            }
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role is null)
+                return NotFound();
+
+            role.Name = model.Name.Trim();
+            await _roleManager.UpdateAsync(role);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        #endregion
+
 
     }
 }

1ba991ee92ebbc489cc65894d483112c314621ea
Hagershawket <hagershawkat@gmail.com>
2024-10-20 05:29:28 +0300

    feat(role): add create action to Role Controller.

--- LinkDev.Talabat.Dashboard/Controllers/RoleController.cs
+++ LinkDev.Talabat.Dashboard/Controllers/RoleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using LinkDev.Talabat.Dashboard.ViewModels.Role;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,31 @@ namespace LinkDev.Talabat.Dashboard.Controllers
             var roles = await _roleManager.Roles.ToListAsync();
 
             return View(roles);
+        }
+
+        #endregion
+
+        #region Create
+
+        [HttpPost] // POST: /Role/Create
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RoleFormViewModel model)
+        {
+            if (!ModelState.IsValid)            
+                return View("Index", model);
+
+            var roleExist = await _roleManager.RoleExistsAsync(model.Name);
+            if (!roleExist)
+            {
+                await _roleManager.CreateAsync(new IdentityRole { Name = model.Name.Trim() });
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ModelState.AddModelError("Name", "This Role is already Exist");
+                return View("Index", await _roleManager.Roles.ToListAsync());
+            }
+
         }
 
         #endregion

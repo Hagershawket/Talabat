b288a7ddc2658821fc8a1fdcbf1bab34cc8beb58
Hagershawket <hagershawkat@gmail.com>
2024-10-20 03:56:34 +0300

    feat(role): add index action to Role Controller.

---
+++ LinkDev.Talabat.Dashboard/Controllers/RoleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkDev.Talabat.Dashboard.Controllers
+{
+    public class RoleController(RoleManager<IdentityRole> _roleManager) : Controller
+    {
+        #region Index
+
+        [HttpGet] // GET: /Role/Index
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+
+            return View(roles);
+        }
+
+        #endregion
+
+
+    }
+}

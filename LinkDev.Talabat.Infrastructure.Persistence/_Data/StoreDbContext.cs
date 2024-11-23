b9999a15492dc12294e67204dabc4664651f2795
Hagershawket <hagershawkat@gmail.com>
2024-11-23 20:06:44 +0200

    Part 03 Order Module - Migrations and Data Seeding

--- LinkDev.Talabat.Infrastructure.Persistence/_Data/StoreDbContext.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/_Data/StoreDbContext.cs
@@ -1,4 +1,5 @@
-﻿using LinkDev.Talabat.Core.Domain.Entities.Products;
+﻿using LinkDev.Talabat.Core.Domain.Entities.Order;
+using LinkDev.Talabat.Core.Domain.Entities.Products;
 using LinkDev.Talabat.Infrastructure.Persistence._Common;
 using System.Reflection;
 
@@ -20,5 +21,8 @@ namespace LinkDev.Talabat.Infrastructure.Persistence.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> Brands { get; set; }
         public DbSet<ProductCategory> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
     }
 }

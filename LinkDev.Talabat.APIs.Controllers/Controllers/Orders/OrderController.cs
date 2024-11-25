24acf4a598bdf24aca43101ac288f89931b54df1
Hagershawket <hagershawkat@gmail.com>
2024-11-25 10:57:34 +0200

    Part 08 GetOrdersForUser, GetOrderForUser, GetDeliveryMethods endpoints

--- LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrderController.cs
+++ LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrderController.cs
@@ -24,5 +24,33 @@ namespace LinkDev.Talabat.APIs.Controllers.Controllers.Orders
 
             return Ok(result);
         }
+
+        [HttpGet]   // GET: /api/Order
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetOrdersForUser()
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            var result = await _serviceManager.OrderService.GetOrdersForUserAsync(buyerEmail!);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]   // GET: /api/Order/{id}
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderById(int id)
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            var result = await _serviceManager.OrderService.GetOrderByIdAsync(buyerEmail!, id);
+
+            return Ok(result);
+        }
+
+        [HttpGet("deliveryMethods")]   // GET: /api/Order/deliveryMethods
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDto>>> GetDeliveryMethods()
+        {
+            var result = await _serviceManager.OrderService.GetDeliveryMethodsAsync();
+
+            return Ok(result);
+        }
     }
 }

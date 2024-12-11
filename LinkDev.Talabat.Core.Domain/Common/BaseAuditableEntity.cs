24c2863b527211bd35e80ce5ca8f4117e374d8f5
Hagershawket <hagershawkat@gmail.com>
2024-12-12 00:44:44 +0200

    fix: resolve issue in BaseAuditableEntityInterceptor for handling entity audits

--- LinkDev.Talabat.Core.Domain/Common/BaseAuditableEntity.cs
+++ LinkDev.Talabat.Core.Domain/Common/BaseAuditableEntity.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace LinkDev.Talabat.Core.Domain.Common
 {
-    public abstract class BaseAuditableEntity<TKey> : BaseEntity<TKey> where TKey : IEquatable<TKey>
+    public interface IBaseAuditableEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+    }
+    public abstract class BaseAuditableEntity<TKey> : BaseEntity<TKey> , IBaseAuditableEntity where TKey : IEquatable<TKey>
     {
         public string CreatedBy { get; set; } = null!;
         public DateTime CreatedOn { get; set; }
